a2f4596e7981511b0de30f25d701759fe26cefe7
bgallois <benjamin@gallois.cc>
2021-10-24 14:31:42 +0200

    [add] add tab switch emacs

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -14,3 +14,18 @@
 (setq confirm-kill-emacs nil)
 
 (remove-hook 'doom-first-buffer-hook #'smartparens-global-mode)
+
+(setq centaur-tabs-set-bar 'over
+      centaur-tabs-set-icons t
+      centaur-tabs-gray-out-icons 'buffer
+      centaur-tabs-height 24
+      centaur-tabs-set-modified-marker t
+      centaur-tabs-style "bar"
+      centaur-tabs-modified-marker "•")
+(map! :leader
+      :desc "Toggle tabs globally" "t c" #'centaur-tabs-mode
+      :desc "Toggle tabs local display" "t C" #'centaur-tabs-local-mode)
+(evil-define-key 'normal centaur-tabs-mode-map (kbd "g <right>") 'centaur-tabs-forward        ; default Doom binding is 'g t'
+                                               (kbd "g <left>")  'centaur-tabs-backward       ; default Doom binding is 'g T'
+                                               (kbd "g <down>")  'centaur-tabs-forward-group
+                                               (kbd "g <up>")    'centaur-tabs-backward-group)
